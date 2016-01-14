87ac73d79a1745b00c77abc91c436e4cc2f8996a
Zack Winchell <zackwinchell@gmail.com>
2016-01-13 19:34:54 -0800

    added Bootstrap Sweetalert and README

--- main.js
+++ main.js
@@ -1,5 +1,22 @@
 "use strict";
 
 $(document).ready(function(){
-  console.log("Things are up and running.");
+  var $newName = $('#newName');
+  var $newEmail = $('#newEmail');
+  var $newPhone = $('#newPhone');
+  var $newAddress = $('#newAddress');
+  var $createNewContact = $('#createNewContact');
+  var $contactTemplate = $('#contactTemplate');
+
+  $createNewContact.click(function(e){
+    debugger;
+    e.preventDefault();
+    newContact()
+  });
+
+  function newContact(){
+
+    var $contact = $contactTemplate.clone().attr('id','');
+    debugger;
+  };
 });
